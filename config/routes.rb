31864b30b6e5b92487ee947f8a8f55ecdd013822
Geogre Doan <mr.cupo97@gmail.com>
2019-08-29 01:24:22 +0700

    config devise, user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, skip: [:sessions]
+  as :user do
+  	get 'signup', to: 'devise/registrations#new'
+	  get 'signin', to: 'devise/sessions#new', as: :new_user_session
+	  post 'signin', to: 'devise/sessions#create', as: :user_session
+	  delete 'signout', to: 'devise/sessions#destroy', as: :destroy_user_session
+	end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#index'
 end
