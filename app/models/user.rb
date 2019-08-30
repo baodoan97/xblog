dd81dd1bf63e1f2879a9dd9d382324496c039713
Geogre Doan <mr.cupo97@gmail.com>
2019-08-30 13:13:27 +0700

    add validate user, customize flash messages, edit login page

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :username, presence: true, length: {minimum: 6, maximum: 12}, uniqueness: true
+
+  validates :first_name, presence: true, length: {minimum: 2, maximum: 10}
+
+  validates :last_name, presence: true, 	length: {minimum: 2, maximum: 10}
 end
--- config/application.rb
+++ config/application.rb
@@ -4,12 +4,14 @@ require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
+
 Bundler.require(*Rails.groups)
 
 module Blog
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.action_view.field_error_proc = Proc.new { |html_tag, instance| html_tag.html_safe }
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
@@ -17,3 +19,5 @@ module Blog
     # the framework and any gems in your application.
   end
 end
+
+
